00ce4ab526a764cf3db7099e030b1bf9ae7bfb65
Sean-Drew <SDrew989@gmail.com>
2021-02-08 14:56:03 -0500

    Functions with objects

--- demo.js
+++ demo.js
@@ -1,6 +1,15 @@
 'use strict'; 
 (function() {
 
-  display('Hello World');
+  let person = {
+    firstName: 'Alex',
+    lastName: 'Johnson',
+    age: 28,
+    isAdult: function() { return person.age >= 18 }
+  }
+
+  display(`${person.firstName} ${person.lastName}`);
+  display(person.age)
+  display(person.isAdult())
 
 })();
