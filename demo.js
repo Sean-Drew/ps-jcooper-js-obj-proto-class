a6ce3ec0642aadfb477b0e41399bb03a66273130
Sean-Drew <SDrew989@gmail.com>
2021-02-08 17:10:21 -0500

    Classes

--- demo.js
+++ demo.js
@@ -1,23 +1,15 @@
 'use strict'; 
 (function() {
 
-  // object literal
-  let person1 = {
-    firstName: 'Jim',
-    lastName: 'Cooper',
-    age: 29
+  class Person {
+    constructor(firstName, lastName, age) {
+      this.firstName = firstName
+      this.lastName = lastName
+      this.age = age
+    }
   }
 
-  // object.create, identical to above, more verbose
-  let person2 = Object.create(
-    Object.prototype,
-    {
-      firstName: { value: 'Jim', enumerable: true, writable: true, configurable: true },
-      lastName: { value: 'Cooper', enumerable: true, writable: true, configurable: true },
-      age: { value: 29, enumerable: true, writable: true, configurable: true}
-    }
-  )
-
-  display(person1, person2)
+  let jim = new Person('Jim', 'Cooper', 29)
+  display(jim)
 
 })();
