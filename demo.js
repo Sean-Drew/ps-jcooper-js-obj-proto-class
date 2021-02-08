3b22f129dd62836c37ee3b4c04b745c862b5b833
Sean-Drew <SDrew989@gmail.com>
2021-02-08 16:02:43 -0500

    Constructor functions

--- demo.js
+++ demo.js
@@ -1,14 +1,17 @@
 'use strict'; 
 (function() {
 
-  function registerUser(firstName, lastName) {
-    let person = {
-      firstName,
-      lastName
-    }
-    display(person)
+  function Person(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.isAdult = function() { return this.age > 21 }
   }
 
-  registerUser('Jim', 'Halpert')
+  let person = new Person('Jim', 'Cooper', 29)
+  display(person)
+
+  let person2 = new Person('Billy', 'Batson', 17)
+  display(person2.isAdult())
 
 })();
